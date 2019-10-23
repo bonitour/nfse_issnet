7bd73e6ded53b04d9f66d39ab8ee12071466862c
Joao Mafra <joaomafra21@gmail.com>
2019-10-23 14:25:24 -0300

    email optional

--- lib/nfse_issnet/entities/tc_contato.rb
+++ lib/nfse_issnet/entities/tc_contato.rb
@@ -2,7 +2,7 @@ module NfseIssnet
   module Entities
     class TcContato < BaseEntity
       attribute :telefone, Types::String.constrained(max_size: 11).optional
-      attribute :email, Types::String.constrained(max_size: 80)
+      attribute :email, Types::String.constrained(max_size: 80).optional
     end
   end
 end
