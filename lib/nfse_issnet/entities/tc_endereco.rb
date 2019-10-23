07e86537fd2f228ceb5df817f0611fe60c3ffbc5
Joao Mafra <joaomafra21@gmail.com>
2019-10-23 14:47:23 -0300

    testando option

--- lib/nfse_issnet/entities/tc_endereco.rb
+++ lib/nfse_issnet/entities/tc_endereco.rb
@@ -4,7 +4,7 @@ module NfseIssnet
       attribute? :endereco, Types::String.constrained(max_size: 125).optional
       attribute? :cep, Types::Coercible::Integer.constrained(lteq: ('9' * 8).to_i).optional
       attribute? :numero, Types::String.constrained(max_size: 10).optional
-      attribute? :complemento, Types::String.constrained(max_size: 60).optional
+      attribute? :complemento, Types::String.constrained(max_size: 60)
       attribute? :bairro, Types::String.constrained(max_size: 60).optional
       attribute? :cidade, Types::String.constrained(max_size: 7)
       attribute? :estado, Types::String.constrained(max_size: 2).optional
